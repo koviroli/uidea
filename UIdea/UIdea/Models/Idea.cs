e700d0f00b8c9178419e52f8164871d2ddaf7d71
Roland Kovács <kovirolig@gmail.com>
2018-05-12 18:12:53 +0200

    Still improving user interface

--- UIdea/UIdea/Models/Idea.cs
+++ UIdea/UIdea/Models/Idea.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,42 @@ namespace UIdea.Models
 {
     public class Idea
     {
+        /// <summary>
+        /// ID of the idea
+        /// </summary>
         public string ID { get; set; }
+        
+        /// <summary>
+        /// ID of the user who created this idea
+        /// </summary>
         public string OwnerID { get; set; }
+
+        /// <summary>
+        /// Title of the idea
+        /// </summary>
+        [Required]
+        [MinLength(3)]
+        [MaxLength(25)]
         public string Title { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required]
+        [MinLength(30)]
+        [MaxLength(500)]
         public string Description { get; set; }
+
+        /// <summary>
+        /// User members' ids
+        /// </summary>
         public string Members { get; set; }
+
+        /// <summary>
+        /// Required skills(eg.: c#, c++, html, qt)
+        /// </summary>
+        [Required]
+        [Display(Name = "Required skills")]
         public string RequiredMembers { get; set; }
     }
 }
