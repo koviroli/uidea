5c4b53bf3f96a94918a177fe4de8432f37b65a27
Roland Kovács <kovirolig@gmail.com>
2018-05-20 12:05:26 +0200

    login with email bug fix

--- UIdea/UIdea/Controllers/AccountController.cs
+++ UIdea/UIdea/Controllers/AccountController.cs
@@ -59,9 +59,10 @@ namespace UIdea.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
+                var user = await _userManager.FindByEmailAsync(model.Email);
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
