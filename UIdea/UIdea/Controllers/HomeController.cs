4bd1861126bef56985f3c4a8db46090136c9afb6
Roland Kovács <kovirolig@gmail.com>
2018-05-30 19:26:37 +0200

    Added users page with lot of initalized users+empty Tags page

--- UIdea/UIdea/Controllers/HomeController.cs
+++ UIdea/UIdea/Controllers/HomeController.cs
@@ -4,21 +4,46 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UIdea.Data;
 using UIdea.Models;
 
 namespace UIdea.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _appDbContext;
+        private readonly UIdeaContext _uideaContext;
+
+        public HomeController(ApplicationDbContext appdbcontext, UIdeaContext uideacontext)
+        {
+            _appDbContext = appdbcontext;
+            _uideaContext = uideacontext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> Users(string searchString, int? page)
         {
             ViewData["Message"] = "Your application description page.";
 
+            var users = from u in _appDbContext.Users
+                        select u;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(s => s.UserName.Equals(searchString));
+            }
+
+            int pageSize = 16;
+            return View(await PaginatedList<ApplicationUser>.CreateAsync(users.AsNoTracking(), page ?? 1, pageSize));
+        }
+
+        public IActionResult Tags()
+        {
             return View();
         }
 
---
+++ UIdea/UIdea/Models/SeedDataApplicationUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UIdea.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace UIdea.Models
+{
+    public static class SeedDataApplicationUser
+    {
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                if (context.Users.Any())
+                    return;
+
+                var defaultAvatar = Path.Combine("wwwroot", "images", "ideaavatar_default_lightbulb_25_25.jpg");
+
+                FileStream image = System.IO.File.OpenRead(@defaultAvatar);
+                
+                for (int i = 0; i < 100; ++i)
+                {
+                    var user = new ApplicationUser()
+                    {
+                        UserName = string.Format("user{0}",i),
+                        Email = string.Format("user{0}@hotmail.com",i),
+                        PasswordHash = "asddsa123",
+                        AvatarImage = new byte[image.Length]
+                    };
+                    image.Read(user.AvatarImage, 0, (int)image.Length);
+                    context.Add(user);
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
